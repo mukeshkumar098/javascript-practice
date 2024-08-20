086087643b48446bc58c8e1509413ac460d73514
mukeshkumar098 <mukeshkemar567@gmail.com>
2024-08-20 21:00:58 +0530

    code

---
+++ addiintion.js
@@ -0,0 +1,22 @@
+let a = 44;
+let b = 10;
+let d = a * b;
+let c = a + b;
+let e = a - b;
+
+console.log(c);
+console.log(d);
+console.log(e);
+
+function sum() {
+    let a = 30;
+    let b = 10;
+    let c = a + b;
+    let d = a * b;
+    let e = a - b;
+    console.log(c)
+    console.log(d);
+    console.log(e);
+
+}
+sum()
---
+++ script.js
@@ -0,0 +1,9 @@
+let name = "mukesh kumar"
+console.log(name);
+
+function func1() {
+    let age = 23;
+    console.log(age);
+
+}
+func1()
